82af147d32cc50226d501c69feb8915ddd29559c
Savva <cabehok@inbox.ru>
2016-07-28 17:45:39 +0300

    changes

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default React.createClass({
 
@@ -17,17 +18,36 @@ export default React.createClass({
 	},
 
 	render() {
-		return (
+		return this.props.winner ?
+			<Winner ref="winner" winner={this.props.winner} /> :
 			<div className="results">
-				{this.getPair().map(entry =>
-					<div key={entry} className="entry">
-					  <h1>{entry}</h1>
-					  <div className="voteCount">
-						{this.getVotes(entry)}
-					  </div>
-					</div>
-				)}
+				<div className="tally">
+					{this.getPair().map(entry =>
+						<div key={entry} className="entry">
+						  <h1>{entry}</h1>
+						  <div className="voteCount">
+							{this.getVotes(entry)}
+						  </div>
+						</div>
+					)}
+				</div>
+				<div className="managment">
+					<button ref="next"
+			                className="next"
+			                onClick={this.props.next}>
+			        	Next
+			        </button>
+				</div>
 			</div>
-		)
 	}
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
